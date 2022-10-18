2b24d840cd7990dc54884e7078e33c83feea1190
akash08-ak <71761210+akash08-ak@users.noreply.github.com>
2022-10-18 14:07:26 +0530

    Update Reg.js

--- FrontEnd/liginapp/src/Reg.js
+++ FrontEnd/liginapp/src/Reg.js
@@ -5,39 +5,29 @@ class Reg extends Component {
   constructor() {
     super();
     this.state = {
-      EmployeeName: '',
-      City: '',
-      Email: '',
+      UserName: '',
       Password: '',
-      Department: ''
+      SequrityQue: ''
     }
 
-    this.Email = this.Email.bind(this);
+    this.UserName = this.UserName.bind(this);
     this.Password = this.Password.bind(this);
-    this.EmployeeName = this.EmployeeName.bind(this);
-    this.Password = this.Password.bind(this);
-    this.Department = this.Department.bind(this);
-    this.City = this.City.bind(this);
-    this.register = this.register.bind(this);
+    this.SequrityQue = this.SequrityQue.bind(this);
+    
   }
 
-  Email(event) {
-    this.setState({ Email: event.target.value })
+  UserName(event) {
+    this.setState({ UserName: event.target.value })
   }
  
-  Department(event) {
-    this.setState({ Department: event.target.value })
-  }
- 
+  
   Password(event) {
     this.setState({ Password: event.target.value })
   }
-  City(event) {
-    this.setState({ City: event.target.value })
+  SequrityQue(event) {
+    this.setState({ SequrityQue: event.target.value })
   }
-  EmployeeName(event) {
-    this.setState({ EmployeeName: event.target.value })
-  }
+
  
   register(event) {
     fetch('http://localhost:51282/Api/login/InsertEmployee', {
@@ -47,11 +37,10 @@ class Reg extends Component {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        EmployeeName: this.state.EmployeeName,
+        UserName: this.state.UserName,
         Password: this.state.Password,
-        Email: this.state.Email,
-        City: this.state.City,
-        Department: this.state.Department
+        SequrityQue: this.state.SequrityQue,
+        
       })
     }).then((Response) => Response.json())
       .then((Result) => {
@@ -79,24 +68,16 @@ class Reg extends Component {
                     </div>
                     <InputGroup className="mb-3">
                       <Input type="text"  
-                      onChange={this.EmployeeName} placeholder="Enter Employee Name" />
+                      onChange={this.UserName} placeholder="Enter User Name " />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <Input type="text"  
-                      onChange={this.Email} placeholder="Enter Email" />
+                      onChange={this.Password} placeholder="Password" />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <Input type="password"  
-                      onChange={this.Password} placeholder="Enter Password" />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <Input type="text"  
-                      onChange={this.City} placeholder="Enter City" />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <Input type="text"  
-                      onChange={this.Department} placeholder="Enter Department" />
-                    </InputGroup>
+                      onChange={this.SequrityQue} placeholder="Enter Equrity Question" />
+                    
                     <Button  onClick={this.register}  
                     color="success" block>Create Account</Button>
                   </Form>
